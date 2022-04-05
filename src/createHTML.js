e76d4c3e1218eb6ec3adca7c91e170f702d6ef98
carternadain <cnadain@gmail.com>
2022-04-05 15:08:56 -0700

    update

--- src/createHTML.js
+++ src/createHTML.js
@@ -48,8 +48,8 @@ const makeIntern = function (intern) {
               <p class="email">Email:<a href="mailto:${intern.email}">${intern.email}</a></p>
               <p class="school">School: ${intern.school}</p>
           </div>
-  </div>
-</div>
+        </div>
+    </div>
   `
 };
 
@@ -82,9 +82,7 @@ createHTML = (data) => {
       }
       
   }
-
   const employeeCards = pageArray.join('')
-
   const generateTeam = newTeamPage(employeeCards); 
   return generateTeam;
 
@@ -99,12 +97,13 @@ return`
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Team Profile</title>
+    <link rel="stylesheet" href="./assets/css/style.css"/>   
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.1.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous"/>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css" />
+</head>
 
-    </head>
 <body>
     <header>
         <nav class="navbar" id="navbar">
